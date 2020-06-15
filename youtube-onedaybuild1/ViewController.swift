8060d9a799b36cbf8e6545dc6e4658d20b7d1324
AxelKv <axel.kvalvagnes@gmail.com>
2020-06-15 23:42:31 +0200

    this one then

---
+++ youtube-onedaybuild1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild1
+//
+//  Created by Axel Kvalvågnes on 14/06/2020.
+//  Copyright © 2020 Axel Kvalvågnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
